fbf3d6d79ec374a90b9af39d8e821adc71a01ba7
arjunmaheshwarii <arjunmaheshwari2005@gmail.com>
2026-03-18 13:48:59 +0530

    Integrated embedding model and similarity search

--- src/vector_store.py
+++ src/vector_store.py
@@ -9,3 +9,10 @@ def create_vector_store(documents):
     embeddings = OpenAIEmbeddings()
     vector_store = FAISS.from_documents(documents, embeddings)
     return vector_store
+
+
+def search_vector_store(vector_store, query):
+    """
+    Perform similarity search.
+    """
+    return vector_store.similarity_search(query, k=3)
